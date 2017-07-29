9c05c358983c5864b5f0581b04a690dee82bf36d
Ishan Mitra <ishanbubby@gmail.com>
2017-07-29 21:41:33 -0200

    Add base LSTM model

--- generate_data.py
+++ generate_data.py
@@ -22,15 +22,3 @@ def get_data_matrix(cuisines):
 		cuisine_scores.append(randint(0, 5))
 	cuisine_scores = np.asarray(cuisine_scores, dtype = float)
 	return cuisines, cuisine_scores
-
-
-
-
-"""
-Main
-"""
-
-if __name__ == "__main__":
-    all_cuisines = get_cuisines()
-    return get_data_matrix(all_cuisines)
-
