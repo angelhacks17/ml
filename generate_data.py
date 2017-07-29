002c897de2b51e452de5dca0979f03e8af691136
Shiv Ansal <ansalshiv@gmail.com>
2017-07-29 13:14:19 -0700

    hardcoded martix

--- generate_data.py
+++ generate_data.py
@@ -1,1 +1,25 @@
 import numpy as np
+from random import randint
+
+
+def get_cuisines(file):
+	return ["Indian", "Chinese"]
+
+def get_data_matrix(cuisines):
+	cuisine_scores = []
+	for cuisine in cuisines:
+		cuisine_scores.append(randint(0, 5))
+	cuisine_scores = np.asarray(cuisine_scores, dtype = float)
+	return cuisines, cuisine_scores
+
+
+
+
+"""
+Main
+"""
+
+if __name__ == "__main__":
+    all_cuisines = get_cuisines("hello")
+    print(get_data_matrix(all_cuisines))
+
