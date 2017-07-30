9bb8414a71af1a7c73dcb62288c1e2a70ee00e50
Shiv Ansal <ansalshiv@gmail.com>
2017-07-29 20:55:31 -0700

    reshaping errors

--- random_tree.py
+++ random_tree.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
 
 LABELS = ["Algeria", "Argentina", "Australia", "Belgium", "Bosnia and Herzegovina","Brazil", "Cameroon", "Chile", "Colombia", "Costa Rica", "Croatia", "Ecuador"]
 LABELS.append(["England", "France", "Germany", "Ghana", "Greece", "Honduras", "Iran", "Italy", "Ivory Coast", "Japan", "Mexico", "Netherlands", "Nigeria", "Portugal"])
@@ -15,10 +17,18 @@ for index, row in df.iterrows():
 	scores = row[3:len(row)-5]
 	for score in scores:
 		if np.isnan(score):
-			user_data.append(np.nan)
+			user_data.append(0)
 		else:
 			user_data.append(float(score))
 	all_data.append(user_data)
 all_data = np.asarray(all_data, dtype=float)
+unique_data = np.asarray(all_data[0], dtype=float)
+data_user = np.reshape(unique_data, len(LABELS))
+
+print(data_user.shape())
+
 print(len(all_data[0]))
 print(len(LABELS))
+print(len(all_data[0]))
+
+RandomForestClassifier().fit(data_user, LABELS)
