c02b35f8b8670d19d3a165f1ed4fda6f7e3b9561
Shiv Ansal <ansalshiv@gmail.com>
2017-07-30 00:31:13 -0700

    find highest correlation

--- random_tree.py
+++ random_tree.py
@@ -8,7 +8,31 @@ LABELS.append(["England", "France", "Germany", "Ghana", "Greece", "Honduras", "I
 LABELS.append(["Russia","South Korea", "Spain", "Switzerland", "United States", "Uruguay", "China", "India", "Thailand", "Turkey", "Cuba", "Ethiopia", "Vietnam", 'Ireland'])
 LABELS = np.hstack(LABELS)
 
-
+def get_correlations(country, data):
+	all_cor = []
+	label_list = LABELS.tolist()
+	index = label_list.index(country)
+	org_score = []
+	for dat in data:
+		org_score.append(dat[index])
+	i = 0
+	while(i < 40):
+		cur_score = []
+		for x in data:
+			cur_score.append(x[i])
+		if index == i:
+			all_cor.append([[-1,-1],[-1,-1]])
+		elif index != i:
+			all_cor.append(np.corrcoef(org_score, cur_score))
+		i = i+1
+	normalized_perc = []
+	for x in all_cor:
+		val = x[0][1]
+		normalized_perc.append(val)
+	print(normalized_perc)
+	max = np.max(normalized_perc)
+	max_index = normalized_perc.index(max)
+	print(LABELS[max_index])
 
 df = pd.read_csv('food-world-cup-data.csv', encoding='latin1')
 all_data = []
@@ -22,13 +46,11 @@ for index, row in df.iterrows():
 			user_data.append(float(score))
 	all_data.append(user_data)
 all_data = np.asarray(all_data, dtype=float)
-unique_data = np.asarray(all_data[0], dtype=float)
-data_user = np.reshape(unique_data, len(LABELS))
 
-print(data_user.shape())
-
-print(len(all_data[0]))
-print(len(LABELS))
-print(len(all_data[0]))
-
-RandomForestClassifier().fit(data_user, LABELS)
+list1 = []
+list2 = []
+for data in all_data:
+	list1.append(data[0])
+	list2.append(data[10])
+	
+get_correlations("England", all_data)
